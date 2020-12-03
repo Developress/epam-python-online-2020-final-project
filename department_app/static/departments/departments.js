8407eca70e6a359dfe75f95eaec6683faae48d63
Olena Hul <olena2002mik@gmail.com>
2020-12-03 12:39:27 +0200

    comments added and refactoring performed

--- department_app/static/departments/departments.js
+++ department_app/static/departments/departments.js
@@ -1,55 +1,76 @@
+// perform a GET request to receive all the departments
 fetch("http://localhost:5000/api/departments")
-        .then( (response) => response.json())
-        .then((data)=> {
-            responseReceived(data);
-        })// output will be the required data
-        .catch( (error) => console.log(error))
+    .then((response) => response.json())
+    .then((data)=> {
+        // if the request was successful, call the function
+        responseReceived(data);
+    })
+    .catch((error) => console.log(error))
 
 function responseReceived(data){
-    console.log(data);
+    // if the department list is empty
     if(data.length == 0){
-        empty = document.getElementById("empty");
+        // set set to empty element to indicate that the department list is empty
+        let empty = document.getElementById("empty");
         let text = document.createTextNode("No departments were found");
         empty.appendChild(text);
     } else {
+        // get the data which will be displayed in table
         let dataToDisplay = formDataToDisplay(data);
+        // get the table to display data
         let table = document.querySelector("table");
+        // generate table, passing the table element, table data, and table headers
         generateTable(table, dataToDisplay, Object.keys(dataToDisplay[0]));
+        // generate table head, passing the table element and table headers
         generateTableHead(table, Object.keys(dataToDisplay[0]).slice(1));
     }
 }
 
-function formDataToDisplay(returnedJson) {
+function formDataToDisplay(data) {
+    // declare an array with objects representing the table rows
     let dataToDisplay = [];
-    for(let i = 0; i < returnedJson.length; i++){
-        object = returnedJson[i];
-        employeeCount = object['employees'].length;
+    for(let i = 0; i < data.length; i++){
+        // get the element of data array
+        let object = data[i];
+        // get the amount of employees related to the current department
+        let employeeCount = object['employees'].length;
+        // declare a variable which will store the average salary of the current department
         let averageSalary = 0;
+        // if employee list is not empty
         if(employeeCount > 0){
             for(let j = 0; j < object['employees'].length; j++){
-            employee = object['employees'][j];
-            averageSalary += employee['salary'];
-        }
+                // get the employee from employee list
+                employee = object['employees'][j];
+                // add the employee's salary to the current sum
+                averageSalary += employee['salary'];
+            }
+            // calculate the average salary
             averageSalary /= employeeCount;
         }
-        department = {
+        // form an object which will be represented in table
+        let department = {
             'id': object['id'],
             'Name': object['name'],
             'Description': object['description'],
             'Employee count': employeeCount,
             'Average salary': averageSalary
         }
+        // push an object to the array
         dataToDisplay.push(department);
     }
     return dataToDisplay;
 }
 
-function generateTableHead(table, data) {
+function generateTableHead(table, headers) {
+    // create the table head
     let thead = table.createTHead();
+    // create the table row
     let row = thead.insertRow();
+    // push edit and delete values to the headers array
     data.push('Edit');
     data.push('Delete');
-    for (let key of data) {
+    for (let key of headers) {
+        // create a table header element and fill it with data
         let th = document.createElement("th");
         let text = document.createTextNode(key);
         th.appendChild(text);
@@ -59,24 +80,35 @@ function generateTableHead(table, data) {
 
 function generateTable(table, data, keys) {
     for(let i = 0; i < data.length; i++) {
+        // get the element from data array
         let element = data[i];
-        console.log(keys)
+        // create a new row
         let row = table.insertRow();
+        // start loop from 1, to omit the id property
         for(let j = 1; j < keys.length; j++) {
+            // create a new cell and fill it with data
             let cell = row.insertCell();
             let text = document.createTextNode(element[keys[j]]);
             cell.appendChild(text);
         }
+        // create a new cell for "Edit" link
         let cell = row.insertCell();
         let a = document.createElement("a");
+        // specify the hyperlink for the element
         a.setAttribute("href", `/departments/edit/${element['id']}`);
+        // bound the text of link and join the element to the table row
         let text = document.createTextNode("Edit");
         a.appendChild(text);
         cell.appendChild(a);
+
+        // create a new cell for "Delete" link
         cell = row.insertCell();
         a = document.createElement("a");
+        // sendDeleteRequest will be called on the element click
         a.setAttribute("onclick", `sendDeleteRequest(${element['id']})`)
+        // to make element clickable
         a.setAttribute("href", "#");
+        // bound the text of link and join the element to the table row
         text = document.createTextNode("Delete");
         a.appendChild(text);
         cell.appendChild(a);
@@ -84,11 +116,14 @@ function generateTable(table, data, keys) {
 }
 
 function sendDeleteRequest(id){
+    // perform a DELETE request to delete the specific department
     fetch(`http://localhost:5000/api/departments/${id}`, {
-        method: 'DELETE'
-    })
-    .then(res => {
-        window.location.href = `/departments/delete/${id}`;
-    }) // or res.json()
-    .then(res => console.log(res))
+            method: 'DELETE'
+        })
+        .then((response) => response.json())
+        .then((data)=> {
+            // when the response is received, redirect to the given page
+            window.location.href = `/departments/delete/${id}`;
+        })
+        .catch((error) => console.log(error))
 }
